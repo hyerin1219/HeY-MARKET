a22eb6e6db12f0d2bb2c8402c34c73e5c33aecf4
hyerin <gpfls662@gmail.com>
2025-03-11 22:23:50 +0900

    메뉴 탭 수정정

--- portfolio/src/components/commons/layout/navigation/LayoutNavigation.presenter.tsx
+++ portfolio/src/components/commons/layout/navigation/LayoutNavigation.presenter.tsx
@@ -4,8 +4,6 @@ import type { ILayoutNavigationUIProps } from './LayoutNavigation.types'
 export default function LayoutNavigationUI(props:ILayoutNavigationUIProps):JSX.Element {
 
     const NAVIGATION_MENUS = [
-        {name: "나의파이어베이스", page:"/myfirebase"},
-        {name: "DOGS", page:"/dogs"},
         {name: "자유게시판", page: "/boards"},
         {name: "중고마켓", page: "/markets"},
         {name: "마이페이지", page: "/mypages"}
